08e3c77a044962a4ceeb8c3e89e6b754071ad231
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 21:24:51 +0300

    Create VariablesTexts.cs
    Contains strings used in both user-facing and internal variables

---
+++ ToDo/ToDo/VariablesTexts.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************
+* Contains string values for user- (e.g. buttons) and non-user-facing   *
+*   texts (e.g. Message values)                                         *
+*************************************************************************/
+
+namespace ToDo
+{
+    public static class VariablesTexts
+    {
+        // User-facing
+        public const string LABEL_HEADER_TEXT = "Text";
+        public const string LABEL_HEADER_DESCRIPTION = "Description";
+
+        public const string TOOLBAR_NAME_SAVE = "Save";
+
+        // Internal
+        public const string PAGE_NAME_ABOUT = "About";
+        public const string PAGE_NAME_BROWSE = "Browse";
+
+        public const string MESSAGE_ADDITEM = "AddItem";
+    }
+}
