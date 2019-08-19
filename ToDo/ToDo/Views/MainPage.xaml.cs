86aecbd34a49bddf5d0e7a2eb57ce577a9d45dbc
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 17:40:55 +0300

    Move page creation to PagesMaker.cs

--- ToDo/ToDo/Views/MainPage.xaml.cs
+++ ToDo/ToDo/Views/MainPage.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using ToDo.Makers;
 
 namespace ToDo.Views
 {
@@ -10,16 +10,8 @@ namespace ToDo.Views
     {
         public MainPage()
         {
-            NavigationPage pageItems = new NavigationPage(new ItemsPage());
-            pageItems.Title = "Browse";
-            pageItems.IconImageSource = "icon_info.png";
-
-            NavigationPage pageAbout = new NavigationPage(new AboutPage());
-            pageAbout.Title = "About";
-            pageAbout.IconImageSource = "icon_menu.png";
-
-            Children.Add(pageItems);
-            Children.Add(pageAbout);
+            Children.Add(PagesMaker.NewNavigationpage(new ItemsPage(), "Browse", "icon_info.png"));
+            Children.Add(PagesMaker.NewNavigationpage(new AboutPage(), "About", "icon_menu"));
         }
     }
 }
