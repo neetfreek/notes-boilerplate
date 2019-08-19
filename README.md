fc19638b8d700d67dd8a784e1530b67932a67a46
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 15:54:29 +0300

    Add about, menu icons to MainPage

---
+++ README.md
@@ -0,0 +1,4 @@
+## Acknowledgements
+
+### Icons
+feathericons (https://github.com/feathericons/feather) by Cole Bemis, released under the MIT License.
--- ToDo/ToDo/Views/MainPage.xaml.cs
+++ ToDo/ToDo/Views/MainPage.xaml.cs
@@ -12,9 +12,11 @@ namespace ToDo.Views
         {
             NavigationPage pageItems = new NavigationPage(new ItemsPage());
             pageItems.Title = "Browse";
+            pageItems.IconImageSource = "icon_info.png";
 
             NavigationPage pageAbout = new NavigationPage(new AboutPage());
             pageAbout.Title = "About";
+            pageAbout.IconImageSource = "icon_menu.png";
 
             Children.Add(pageItems);
             Children.Add(pageAbout);
