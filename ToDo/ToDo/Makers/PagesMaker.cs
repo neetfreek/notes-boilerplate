571993beab7f879d2dea7de4257eb6876c53b2de
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 17:35:00 +0300

    Create PagesMaker.cs
    Responsible for creating, returning new page objects to callers

---
+++ ToDo/ToDo/Makers/PagesMaker.cs
@@ -0,0 +1,23 @@
+﻿/**************************************************************************
+* Creates, returns new page objects                                    *
+***************************************************************************/
+using Xamarin.Forms;
+
+namespace ToDo.Makers
+{
+    public static class PagesMaker
+    {
+        public static NavigationPage NewNavigationpage(ContentPage pageToNavigateTo, string title, string iconImageSource)
+        {
+            var pageType = pageToNavigateTo.GetType();
+
+            NavigationPage navigationPage = new NavigationPage(pageToNavigateTo)
+            {
+                Title = title,
+                IconImageSource = iconImageSource,
+            };
+
+            return navigationPage;
+        }
+    }
+}
