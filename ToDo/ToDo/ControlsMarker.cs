d8574e78b3a7e96b28892536a14f7a74d1bf63a4
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 17:20:44 +0300

    Create ControlsMarker.cs
    Responsible for creating and returning control objects for callers.

---
+++ ToDo/ToDo/ControlsMarker.cs
@@ -0,0 +1,32 @@
+﻿/**************************************************************************
+* Creates, returns new control objects                                    *
+***************************************************************************/
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public static class ControlsMarker
+    {
+        public static Label NewLabel(string text, double fontSize)
+        {
+            Label label = new Label
+            {
+                Text = text,
+                FontSize = fontSize,
+            };
+
+            return label;
+        }
+
+        public static StackLayout NewStackLayout(double padding, double spacing)
+        {
+            StackLayout stackLayout = new StackLayout
+            {
+                Spacing = spacing,
+                Padding = padding,
+            };
+
+            return stackLayout;
+        }
+    }
+}
