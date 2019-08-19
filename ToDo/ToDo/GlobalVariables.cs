9cafceea0b7ddd203e06136df12d3fc02f03785a
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-19 18:47:41 +0300

    Create GlobalVariables.cs
    Contains commonly-used CONST variables used throughout the application

---
+++ ToDo/ToDo/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿/**************************************************************************
+* Contains commonly-used CONST variables used throughout the application  *
+***************************************************************************/
+
+namespace ToDo
+{
+    public static class GlobalVariables
+    {
+        public const double TEXT_SIZE_SMALL = 14;
+        public const double TEXT_SIZE_MEDIUM = 18;
+
+        public const double PADDING_DEFAULT = 15;
+        public const double SPACING_DEFAULT = 20;
+    }
+}
