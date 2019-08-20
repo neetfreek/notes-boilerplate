b2728361681b81976fb9e30525bd29e5f9b26454
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-20 11:59:07 +0300

    Update VariablesGlobal.cs
    Rename defualt text size values, add small

--- ToDo/ToDo/VariablesGlobal.cs
+++ ToDo/ToDo/VariablesGlobal.cs
@@ -6,8 +6,9 @@ namespace ToDo
 {
     public static class VariablesGlobal
     {
-        public const double TEXT_SIZE_SMALL = 14;
-        public const double TEXT_SIZE_MEDIUM = 18;
+        public const double TEXT_SIZE_SMALL = 12;
+        public const double TEXT_SIZE_MEDIUM = 14;
+        public const double TEXT_SIZE_LARGE = 18;
 
         public const double PADDING_DEFAULT = 15;
         public const double SPACING_DEFAULT = 20;
