302c9324669d1c8dfcb72e871b88841d1c06673d
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-20 11:42:44 +0300

    Update reference to renamed ElementMaker.cs

--- ToDo/ToDo/Views/MainPage.cs
+++ ToDo/ToDo/Views/MainPage.cs
@@ -11,8 +11,8 @@ namespace ToDo.Views
     {
         public MainPage()
         {
-            Children.Add(PageMaker.NewNavigationpage(new ItemsPage(), VariablesTexts.PAGE_NAME_BROWSE, "icon_info.png"));
-            Children.Add(PageMaker.NewNavigationpage(new AboutPage(), VariablesTexts.PAGE_NAME_ABOUT, "icon_menu"));
+            Children.Add(ElementMaker.NewNavigationpage(new ItemsPage(), VariablesTexts.PAGE_NAME_BROWSE, "icon_info.png"));
+            Children.Add(ElementMaker.NewNavigationpage(new AboutPage(), VariablesTexts.PAGE_NAME_ABOUT, "icon_menu"));
         }
     }
 }
