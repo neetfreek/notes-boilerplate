ad45e10f1f38167964e00bfb69ea1f7f53bf8d3a
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-20 20:41:13 +0300

    Update VariablesTexts.cs

--- ToDo/ToDo/VariablesTexts.cs
+++ ToDo/ToDo/VariablesTexts.cs
@@ -8,9 +8,12 @@ namespace ToDo
     public static class VariablesTexts
     {
         // User-facing
+        public const string BUTTON_NAME_VIEW = "View";
+
         public const string LABEL_HEADER_TEXT = "Text";
         public const string LABEL_HEADER_DESCRIPTION = "Description";
 
+        public const string TOOLBAR_NAME_ADD = "Add";
         public const string TOOLBAR_NAME_SAVE = "Save";
 
         // Internal
@@ -18,5 +21,10 @@ namespace ToDo
         public const string PAGE_NAME_BROWSE = "Browse";
 
         public const string MESSAGE_ADDITEM = "AddItem";
+
+        // Image resource paths
+        public const string ICON_PATH_ADD = "icon_add.png";
+        public const string ICON_PATH_INFO = "icon_info.png";
+        public const string ICON_PATH_MENU = "icon_menu.png";
     }
 }
