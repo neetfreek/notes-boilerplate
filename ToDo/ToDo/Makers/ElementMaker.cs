ad67db917a1ea285c7b2ed796c3198e4fc63c82c
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-20 11:42:17 +0300

    Replace PageMaker.cs with ElementMaker.cs, create ItemsViewMaker.cs

---
+++ ToDo/ToDo/Makers/ElementMaker.cs
@@ -0,0 +1,36 @@
+﻿/************************************************************************************
+* Creates, returns new Page class derived objects like NavigationPage               *
+* These objects occupy the entire screen, and represent the top level               *
+*   UI elements in applications.                                                    *
+* https://docs.microsoft.com/en-us/dotnet/api/Xamarin.Forms.Page?view=xamarin-forms *
+*************************************************************************************/
+using Xamarin.Forms;
+
+namespace ToDo.Makers
+{
+    public static class ElementMaker
+    {
+        public static ViewCell NewViewCell(View view)
+        {
+            ViewCell viewCell = new ViewCell()
+            {
+                View = view,
+            };            
+
+            return viewCell;
+        }
+
+        public static NavigationPage NewNavigationpage(ContentPage pageToNavigateTo, string title, string iconImageSource)
+        {
+            var pageType = pageToNavigateTo.GetType();
+
+            NavigationPage navigationPage = new NavigationPage(pageToNavigateTo)
+            {
+                Title = title,
+                IconImageSource = iconImageSource,
+            };
+
+            return navigationPage;
+        }
+    }
+}
---
+++ ToDo/ToDo/Makers/ItemsViewMaker.cs
@@ -0,0 +1,22 @@
+﻿/********************************************************************************************
+* Creates, returns new ItemsView<TVisual> class derived objects, namely ListViews           *                                             
+* These objects are used display collections of data as a vertical list.                    *
+* https://docs.microsoft.com/en-us/dotnet/api/xamarin.forms.itemsview-1?view=xamarin-forms  *
+*********************************************************************************************/
+using Xamarin.Forms;
+
+namespace ToDo.Makers
+{
+    public static class ItemsViewMaker
+    {
+        public static ListView NewListView(DataTemplate dataTemplate)
+        {
+            ListView listView = new ListView()
+            {
+                ItemTemplate = dataTemplate,
+            };
+
+            return listView;
+        }
+    }
+}
