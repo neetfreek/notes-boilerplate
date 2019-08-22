dd4214e4fe603d2620dcf0a5c3e8751925a66055
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-22 11:24:59 +0300

    Create FontSizes.cs
    Converts font size CONST string values to Device NamedSizes

---
+++ ToDo/ToDo/FontSizes.cs
@@ -0,0 +1,30 @@
+﻿/****************************************************************
+* Converts font size CONST string values to Device NamedSizes   *
+*****************************************************************/
+using System;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public static class FontSizes
+    {
+        public static double DeviceNamedFontSize(string fontSize, Element targetElement)
+        {
+            double namedSize = 0;
+            switch (fontSize)
+            {
+                case VariablesGlobal.TEXT_SIZE_SMALL:
+                    namedSize = Device.GetNamedSize(NamedSize.Small, targetElement);
+                    break;
+                case VariablesGlobal.TEXT_SIZE_MEDIUM:
+                    namedSize = Device.GetNamedSize(NamedSize.Medium, targetElement);
+                    break;
+                case VariablesGlobal.TEXT_SIZE_LARGE:
+                    namedSize = Device.GetNamedSize(NamedSize.Large, targetElement);
+                    break;
+            }
+
+            return namedSize;
+        }
+    }
+}
