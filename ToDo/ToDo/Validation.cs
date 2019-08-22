27c924f2eab30223cbeb353fa231e37aa8d60575
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-22 11:25:27 +0300

    Create Validation.cs
    Validates strings

---
+++ ToDo/ToDo/Validation.cs
@@ -0,0 +1,22 @@
+﻿/**************************************************************************
+* Contains methods for validating various strings e.g. colour hex codes   *
+***************************************************************************/
+using System.Text.RegularExpressions;
+
+namespace ToDo
+{
+    public static class Validation
+    {
+        public static bool HexCodeValid(string hexCode)
+        {
+            string hexCodeConsidered = hexCode;
+
+            if (hexCodeConsidered.Length == 7 && hexCodeConsidered[0] == '#' && Regex.IsMatch(hexCodeConsidered.Substring(1, -1), @"^[a-zA-Z0-9]+$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
