6d5e267febdd022e4c47bd6fd9fbddbc26109fd0
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-22 16:34:34 +0300

    Create DataStorage.cs, Add CreateFileAsync,  CheckFileExistsAsync, DeleteFileAsync methods
    Class responsible for local device storage, reading, writing data.

---
+++ ToDo/ToDo/Services/DataStorage.cs
@@ -0,0 +1,43 @@
+﻿/************************************************************************************
+* Responsible for creating, accessing, deleting, local device storage folders,      *
+*   reading from, writing to local device storage files.                            *
+* https://github.com/dsplaisted/PCLStorage                                          *
+*************************************************************************************/
+using System.Threading.Tasks;
+using PCLStorage;
+
+namespace ToDo.Services
+{
+    class DataStorage
+    {
+        IFolder rootFolder = FileSystem.Current.LocalStorage;
+
+
+        private async Task<IFile> CreateFileAsync()
+        {
+            IFile fileSave = await rootFolder.CreateFileAsync("todoNotes", CreationCollisionOption.ReplaceExisting);
+
+            return fileSave;
+        }
+
+        private async void DeleteFileAsync(string fileName, IFolder rootFolder = null)
+        {
+            if (await CheckFileExistsAsync(fileName))
+            {
+                IFile fileToDelete = await rootFolder.GetFileAsync(fileName);
+                await fileToDelete.DeleteAsync();
+            }
+        }
+
+        private async Task<bool> CheckFileExistsAsync(string fileName)
+        {
+            IFolder folder = rootFolder ?? FileSystem.Current.LocalStorage;
+            ExistenceCheckResult fileExists = await folder.CheckExistsAsync(fileName);
+            if (fileExists == ExistenceCheckResult.FileExists)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
