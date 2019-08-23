e9c846810978a637b85aac7f55f3459903ea831b
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-23 14:03:48 +0300

    Create ItemSerialization
    Converts Item objects to JSON string format and vice-versa

---
+++ ToDo/ToDo/Services/ItemSerialization.cs
@@ -0,0 +1,83 @@
+﻿/**************************************************************************
+* Handles conversion of Item objects to JSON string format and vice-versa *
+***************************************************************************/
+using System;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using ToDo.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace ToDo.Services
+{
+    public static class ItemSerialization
+    {
+        public async static void WriteAllData(ObservableCollection<Item> items)
+        {
+            string dataFinal = ItemsToString(items);
+            await DataStorage.WriteToFileAsync(VariablesGlobal.NAME_FILE_DATA, dataFinal);
+        }
+
+        public async static Task<ObservableCollection<Item>> ReadAllData(string fileName)
+        {
+            string itemsString = await DataStorage.ReadFromFileAsync(fileName);
+
+            return StringsToItems(itemsString);
+        }
+
+        public static ObservableCollection<Item> StringsToItems(string itemsString)
+        {
+            string itemsAll = itemsString;
+            List<string> listItemsString = new List<string>();
+            int itemsAllLength = itemsAll.Length;
+            ObservableCollection<Item> items = new ObservableCollection<Item>();
+
+            while (itemsAllLength > 0)
+            {
+                int positionEndLine = itemsAll.IndexOf("\n");
+                listItemsString.Add(itemsAll.Substring(0, positionEndLine));
+                items.Add(StringToItem(itemsAll.Substring(0, positionEndLine)));
+                itemsAll = itemsAll.Remove(0, (itemsAll.Substring(0, positionEndLine).Length) + 1);
+                itemsAllLength = itemsAll.Length;
+            }
+            return items;
+        }
+
+
+        private static string ItemsToString(ObservableCollection<Item> items)
+        {
+            string dataFinal = "";
+
+            foreach (Item item in items)
+            {
+                dataFinal += ItemToString(item) + Environment.NewLine;
+            }
+
+            return dataFinal;
+        }
+
+        private static string ItemToString(Item item)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Item));
+            serializer.WriteObject(stream, item);
+            
+            byte[] itemJson = stream.ToArray();
+            stream.Close();
+            string itemString = Encoding.UTF8.GetString(itemJson, 0, itemJson.Length);
+
+            return itemString;
+        }
+
+        private static Item StringToItem(string itemString)
+        {
+            Item item = new Item();
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(itemString));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(item.GetType());
+
+            return serializer.ReadObject(stream) as Item;
+        }
+    }
+}
